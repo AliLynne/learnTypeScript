ed23ad98711ba90be2fb9ae49ec21e3e8dcea3ea
AliLynne <alilynne.thompson@gmail.com>
2020-02-16 06:48:59 -0800

    completed chapter 1 & 2

--- todos/src/index.ts
+++ todos/src/index.ts
@@ -1,6 +1,7 @@
 import { TodoItem } from "./TodoItem";
-import { TodoCollection } from "./todoCollection";
+import { TodoCollection } from "./TodoCollection";
 import * as inquirer from "inquirer";
+import { JsonTodoCollection } from "./jsonTodoCollection";
 
 let todos: TodoItem[] = [
   new TodoItem(1, "Buy Flower"),
@@ -9,7 +10,7 @@ let todos: TodoItem[] = [
   new TodoItem(4, "Call Joe", true)
 ];
 
-let collection: TodoCollection = new TodoCollection("Ali", todos);
+let collection: TodoCollection = new JsonTodoCollection("Ali", todos);
 let showCompleted = true;
 function displayTodoList(): void {
   console.log(
